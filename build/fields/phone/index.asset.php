24333604865cc708cc38b771defa9a71f28e7937
Alexus450 <Samarschi@gmail.com>
2024-02-08 12:27:56 +0200

    Fixed issue with the checkbox input required

--- build/fields/phone/index.asset.php
+++ build/fields/phone/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e298d93a29affdb5f2a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '41caba20fc50497395cc');
