ba171eb4936eb7a929ddd007cfe2287cbcf28e76
Alexus450 <Samarschi@gmail.com>
2024-04-09 18:49:22 +0300

    Update script.js
    Added function to remove attr required if at least on of the multi checkbox is ticked

--- dist/assets/frontend/js/script.js
+++ dist/assets/frontend/js/script.js
@@ -26,3 +26,51 @@ function wpzf_submit(token) {
         jQuery('form.wpzoom-forms_form').trigger('submit');
     }
 }
+
+(function ($) {
+    'use strict';
+
+    $('.wp-block-wpzoom-forms-multi-checkbox-field').each(function() {
+
+        var $this = $(this);
+        var $checkboxes = $this.find('input[type="checkbox"]:required');
+    
+        // Check if any checkbox is initially checked
+        var anyChecked = false;
+        $checkboxes.each(function() {
+            if ($(this).prop('checked')) {
+                anyChecked = true;
+                return false; // Exit the loop early since we found a checked checkbox
+            }
+        });
+    
+        // If any checkbox is initially checked, remove the required attribute
+        if (anyChecked) {
+            $checkboxes.removeAttr('required');
+        }
+    
+        $checkboxes.on('change', function() {
+            var atLeastOneChecked = false;
+    
+            // Check if at least one checkbox is checked
+            $checkboxes.each(function() {
+                if ($(this).prop('checked')) {
+                    atLeastOneChecked = true;
+                    return false; // Exit the loop early since we found a checked checkbox
+                }
+            });
+    
+            // If at least one checkbox is checked, remove the required attribute
+            if (atLeastOneChecked) {
+                $checkboxes.removeAttr('required');
+            } else {
+                // If no checkbox is checked, add the required attribute back
+                $checkboxes.attr('required', 'required');
+            }
+        });
+    
+    });
+    
+    
+
+})(jQuery);
