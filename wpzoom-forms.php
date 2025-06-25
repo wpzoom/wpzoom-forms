be7bc36579e389ec5dd2842be717cb5e748b7ae3
Pavel Ciorici <cioricip@gmail.com>
2025-06-25 17:53:29 +0200

    v1.3.2

--- wpzoom-forms.php
+++ wpzoom-forms.php
@@ -13,7 +13,7 @@
  * Description: Simple, user-friendly contact form plugin for WordPress that utilizes Gutenberg blocks for easy form building and customization.
  * Author:      WPZOOM
  * Author URI:  https://www.wpzoom.com
- * Version:     1.3.1
+ * Version:     1.3.2
  * License:     GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
