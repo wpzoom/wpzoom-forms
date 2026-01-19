52213260df74208c781366376c42010ebecac1d8
Qara Yahya <qara.yahya@gmail.com>
2026-01-19 22:04:50 +0100

    Refactor checkbox save component structure for improved readability

--- src/fields/checkbox/save.js
+++ src/fields/checkbox/save.js
@@ -1,3 +1,7 @@
+/**
+ * WordPress dependencies
+ */
+
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
@@ -5,25 +9,27 @@ const Save = ( { attributes } ) => {
 	const blockProps = useBlockProps.save();
 	const { id, name, defaultValue, label, showLabel, required } = attributes;
 
-	return <>
-		<input
-			type="checkbox"
-			name={ id }
-			id={ id }
-			checked={ true == defaultValue }
-			onChange={ e => {} }
-			required={ !! required }
-			{ ...blockProps }
-		/>
-
-		{ showLabel && <label htmlFor={ id }>
-			<RichText.Content
-				tagName="span"
-				value={ label }
+	return (
+		<div { ...blockProps }>
+			<input
+				type="checkbox"
+				name={ id }
+				id={ id }
+				checked={ true == defaultValue }
+				onChange={ e => {} }
+				required={ !! required }
+				
 			/>
-			{ required && <sup className="wp-block-wpzoom-forms-required">{ __( '*', 'wpzoom-forms' ) }</sup> }
-		</label> }
-	</>;
+
+			{ showLabel && <label htmlFor={ id }>
+				<RichText.Content
+					tagName="span"
+					value={ label }
+				/>
+				{ required && <sup className="wp-block-wpzoom-forms-required">{ __( '*', 'wpzoom-forms' ) }</sup> }
+			</label> }
+		</div>
+	);
 };
 
 export default Save;
