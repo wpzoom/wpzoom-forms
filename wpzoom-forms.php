c55e18c120afd3358bc2c2b0c34154480f92c660
Qara Yahya <qara.yahya@gmail.com>
2026-01-23 23:46:02 +0100

    Update upsell badge styles

--- wpzoom-forms.php
+++ wpzoom-forms.php
@@ -765,7 +765,7 @@ class WPZOOM_Forms {
 		add_submenu_page(
 			'edit.php?post_type=wpzf-form',
 			$page_title,
-            '<span style="color:#3496fe; font-weight: 600;">' . esc_html__( 'UPGRADE', 'wpzoom-forms' ) . ' &rarr; <span class="wpz-premium-badge" style="background-color: #3496fe; color: #fff; margin-left: 3px; font-size: 11px; min-height: 16px;  border-radius: 8px; display: inline-block; font-weight: 600; line-height: 1.6; padding: 0 8px">PRO</span></span>',
+            '<span style="color:#3496fe; font-weight: 600;">' . esc_html__( 'UPGRADE', 'wpzoom-forms' ) . ' &rarr; <span class="wpz-premium-badge" style="background-color: #3496fe; color: #fff; margin-left: 3px; font-size: 9px; min-height: 14px; border-radius: 3px; display: inline-block; font-weight: 600; line-height: 1.4; padding: 0 5px">PRO</span></span>',
 			'manage_options',
 			'wpzf-upsell',
 			array( $this, 'render_upsell_page' )
@@ -774,7 +774,7 @@ class WPZOOM_Forms {
 		add_submenu_page(
 			'edit.php?post_type=wpzf-form',
 			esc_html__( 'Import & Export', 'wpzoom-forms' ),
-			esc_html__( 'Import & Export', 'wpzoom-forms' ),
+			esc_html__( 'Import & Export', 'wpzoom-forms' ) . ' <span class="wpz-premium-badge" style="background-color: #3496fe; color: #fff; margin-left: 3px; font-size: 9px; min-height: 14px; border-radius: 3px; display: inline-block; font-weight: 600; line-height: 1.4; padding: 0 5px">PRO</span>',
 			'edit_posts',
 			'wpzf-import',
 			array( $this, 'render_import_export_upsell_page' )
