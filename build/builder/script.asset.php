27b3c8ba9dc96a4a94eb1a5564ef8279b6dc8098
Qara Yahya <qara.yahya@gmail.com>
2026-05-19 16:27:07 +0100

    style: full-page builder

--- build/builder/script.asset.php
+++ build/builder/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e7fcdd83f05551704cd3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7c09343a1a2a3c5456e8');
