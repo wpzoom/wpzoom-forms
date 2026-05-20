5e8f0a8cbfb1270b79faf72e8eb2271478b787ee
Qara Yahya <qara.yahya@gmail.com>
2026-05-20 12:48:59 +0100

    style: hide the admin bar

--- build/builder/script.asset.php
+++ build/builder/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '94ef3da249eacf141a0e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '59742fd6f3f9e61f05df');
