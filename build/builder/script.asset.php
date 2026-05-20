91c5b55d1f4483bb503fe181fee36c36664cfe93
Qara Yahya <qara.yahya@gmail.com>
2026-05-20 18:21:38 +0100

    style: custom-styled checkbox, radio and select

--- build/builder/script.asset.php
+++ build/builder/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a4d92adf321eaa43d0ed');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '82db35e68b5e0c13d9c1');
