e7c56e4739b6aab6fc8e40829229009a4f24c11b
Qara Yahya <qara.yahya@gmail.com>
2026-05-21 09:58:24 +0100

    style: small fixes

--- build/builder/script.asset.php
+++ build/builder/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '82db35e68b5e0c13d9c1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '758e4251ae032637bf79');
