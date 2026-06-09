2155c02c3fa0efaeb232a1399a091e102ef16456
Pavel Ciorici <cioricip@gmail.com>
2026-06-09 08:58:12 +0200

    .

--- build/builder/script.asset.php
+++ build/builder/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-preferences', 'wp-primitives'), 'version' => '2e43fc584cefc734616a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-preferences', 'wp-primitives'), 'version' => '696530d2dd2362c97da8');
